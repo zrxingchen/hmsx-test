1b19497eaf4e7cde1f488dd02603e92098a04810
Zuiru <2425277916@qq.com>
2020-04-28 18:57:54 +0800

    自定义url处理类&&数据库创建

--- web/static/js/user/login.js
+++ web/static/js/user/login.js
@@ -1,43 +1,46 @@
 ;
 var user_login_ops = {
     init:function(){
-        this.eventBind();
         this.eventBind()
     },
     eventBind:function(){
         $(".login_wrap .do-login").click(function(){
+            // btn
+            var btn_target = $(this)
+            if (btn_target.hasClass("disabled")) {
+                alert('请不要重复提交')
+                return;
+            }
             var login_name = $(".login_wrap input[name=login_name]").val()
             var login_pwd = $(".login_wrap input[name=login_pwd]").val()
 
-            // 前端校检
-            if (login_name == undefined || login_name.length < 1){
-                alert('请输入正确的用户名')
-                return
-            }
-            if (login_pwd == undefined || login_pwd.length < 1){
-                alert('请输入正确的密码')
-                return
-            }
-
-            $.ajax({
-                url:common_ops.buildUrl("/user/login"),
-                type:"POST",
-                data:{'login_name':login_name,'login_pwd':login_pwd},
-                dataType:'json',
-                success:function(resp){
-                    console.log(resp)
-                    alert(resp.msg)
-                }
-            })
-        })
             console.log(login_name)
             console.log(login_pwd)
+            // 前端校检 不为空，长度不小于1
+            if(login_name == undefined || login_name.length < 1){
+                alert('您输入的用户名为空')
+                return;
+            }
+            if(login_pwd == undefined || login_pwd.length < 1){
+                alert('您输入的密码为空')
+                return;
+            }
+
+            btn_target.addClass("disabled")
+
+            // Ajax 前后端分离的 ,数据格式是json {key:value},
+            // 前端发送的数据是json格式的，所以后端response 返回的数据也得是json格式的
+            $.ajax({
+                url:"/user/login",
+                type:"POST",
+                data:{"login_name":login_name,"login_pwd":login_pwd},
+                dataType:"json",
+                success:function(response){
+                    btn_target.removeClass("disabled")
+                    console.log(response)
+                }
+            })
+
         })    
     }
 }
-
-
-$(document).ready(function(){
-    user_login_ops.init();
-    user_login_ops.init()
-}) 
