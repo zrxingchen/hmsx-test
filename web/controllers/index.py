2a7cec0e91c2268308cf885a87e1c4a510adc673
Zuiru <2425277916@qq.com>
2020-04-29 06:25:10 +0800

    首页路由设置，页面加载，静态资源加载

---
+++ web/controllers/index.py
@@ -0,0 +1,7 @@
+from flask import Blueprint,render_template
+
+router_index = Blueprint('index_page',__name__)
+
+@router_index.route("/")
+def index():
+    return render_template("index/index.html") 
