795d9fd7b776b349af7739f5779f9eaa2fd6c527
Zuiru <2425277916@qq.com>
2020-04-28 17:46:12 +0800

    login登录页面

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,8 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 router_user = Blueprint('user_page',__name__)
 
 @router_user.route('/login')
 
 def login():
-    return 'login页面'
+    return render_template('user/login.html')
+
+
+@router_user.route('/logout')
+
+def logout():
+    return 'logout页面'
+
+
+
+
+
+@router_user.route('/edit')
+
+def edit():
+    return 'ledit页面'
+
+
+
+@router_user.route('/reset-pwd')
+
+def resetPwd():
+    return 'reset-pwd页面'
+
+
+
