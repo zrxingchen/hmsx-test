4d1e184d118f70627c2fbe85c4e65aab4e48e182
Zuiru <2425277916@qq.com>
2020-04-28 16:05:50 +0800

    user蓝图

---
+++ manager.py
@@ -0,0 +1,21 @@
+from application import app,manager
+from flask_script import Server
+import urls
+
+# web 服务器
+manager.add_command('runserver',Server(host='localhost',port=5000,use_debugger=True,use_reloader=True))
+
+
+def main():
+    manager.run()
+
+
+
+if __name__ == "__main__":
+    try:
+
+        import sys
+        sys.exit(main())
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
